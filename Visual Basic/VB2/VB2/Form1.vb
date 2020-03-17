1cfbad0a4c930c7ef60de280af4cc1ec6556f43e
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-18 08:47:25 +0900

    Create Form1.vb

---
+++ Visual Basic/VB2/VB2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim sum1 As Integer = 0
+    Dim sum2 As Integer = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sum1 = sum1 + 1
+        TextBox1.Text = sum1
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        sum2 = sum2 + 10
+        TextBox2.Text = sum2
+    End Sub
+End Class
