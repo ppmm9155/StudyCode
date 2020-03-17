b13392d1ed58706a9099571f8d50fc203daa344f
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-18 08:46:06 +0900

    Create Form1.vb

---
+++ Visual Basic/VB1/VB1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+  
+    Private Sub btnoutput_Click(sender As Object, e As EventArgs) Handles btnoutput.Click
+        Dim strHello As String
+        strHello = "안녕하세요 " + TextBox1.Text + "님"
+        TextBox2.Text = strHello
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        End
+
+    End Sub
+End Class
