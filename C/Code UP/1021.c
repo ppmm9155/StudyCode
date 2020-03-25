0ea6eef662fdb4c8c5ec5a6cc93855504bc510fd
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-26 00:40:14 +0900

    Create 1021.c

---
+++ C/Code UP/1021.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void)
+{
+	char data[51]="";
+	scanf("%s",data);
+	printf("%s",data);
+	return 0; 
+	/*
+	C언어 출력타입
+%d: 10진수(정수형)
+
+%f: 실수형
+%e: 지수형
+%o: 8진수
+%x: 16진수
+%u: 부호없는 10진수
+%g: 실수형 자동출력
+%p: 포인터의 주소
+%c: 하나의 문자로 출력
+%s: 문자열
+	*/
+}
