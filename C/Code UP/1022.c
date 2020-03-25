352ffa3ac9a2eadf962cbabbcf961b67b833e73e
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-26 01:35:48 +0900

    Update 1022.c

--- C/Code UP/1022.c
+++ C/Code UP/1022.c
@@ -5,5 +5,5 @@ int main (void)
 	char data[2001]=""; // 공백이 포함된 문장 stdin  
 	fgets(data,2000,stdin); //fgets( ) 를 사용하면 공백문자가 포함되어잇는 문장을 입력받아 저장할 수 있다
 	printf("%s",data);
-	return 0; 
+	return 0;  
 }
