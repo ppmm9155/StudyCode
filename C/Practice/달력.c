c0ccb299b7fb12c001053eccb73e1331312885a3
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-27 01:25:50 +0900

    Create 달력.c

---
+++ C/Practice/달력.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("일\t월\t화\t수\t목\t금\t토\n");
+	printf("1\t2\t3\t4\t5\t6\t7\n8\t9\t10\t11\t12\t13\t14\n15\t16\t17\t18\t19\t20\t21\n");
+	printf("22\t23\t24\t25\t26\t27\t28\n29\t30\t31\n");
+	return 0;
+}
