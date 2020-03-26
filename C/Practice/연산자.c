eccb352e917a3b4dab20edee134a21af0fb6ca0b
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-27 01:26:31 +0900

    Create 연산자.c

---
+++ C/Practice/연산자.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("%d+%d=%d\n",7,8,7+8);
+	printf("%d-%d=%d\n",7,8,7-8);
+	printf("%d*%d=%d\n",7,8,7*8);
+	printf("%d/%d=%d\n",7,8,7/8);
+	return 0;
+}
