b9c5a5a774a7c8c334255488fa926fdc6bc69aae
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-29 23:57:22 +0900

    Update 1024.c

--- C/Code UP/1024.c
+++ C/Code UP/1024.c
@@ -5,9 +5,9 @@ int main (void)
 	int i=0; 
 	char data[30]="";
 	scanf("%s",data);
-	for(i=0;data[i]!='\0';i++) {
+	for(i=0;data[i]!='\0';i++) 
+	{
 		printf("\'%c\'\n",data[i]);
 }
-
 	return 0;
 }
