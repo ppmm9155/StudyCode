f03a74e4cc204793d4d35abc77ee121e99bfda41
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-03-31 00:09:17 +0900

    Create 구구단 3단.c

---
+++ C/Assignment/구구단 3단.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main (void)
+{
+	printf("%d*%d=%d\n",3,1,3*1);
+	printf("%d*%d=%d\n",3,2,3*2);
+	printf("%d*%d=%d\n",3,3,3*3);
+	printf("%d*%d=%d\n",3,4,3*4);
+	printf("%d*%d=%d\n",3,5,3*5);
+	printf("%d*%d=%d\n",3,6,3*6);
+	printf("%d*%d=%d\n",3,7,3*7);
+	printf("%d*%d=%d\n",3,8,3*8);
+	printf("%d*%d=%d\n",3,9,3*9);
+	return 0;
+}
