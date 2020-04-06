b2bd1cff67896c593545ed42c6d3ca1d68ba0130
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-04-07 00:00:36 +0900

    Create circle_area.c

---
+++ C/Practice/circle_area.c
@@ -0,0 +1,14 @@
+#include <Stdio.h>
+
+int main (void)
+{
+	double radius;
+	double area;
+	
+	printf("원의 면적을 입력하시오:");
+	scanf("%lf",&radius);
+	
+	area=3.141592*radius*radius;
+	printf("원의 면적: %f\n",area);
+	return 0;
+}
