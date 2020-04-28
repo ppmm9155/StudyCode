711c1dbb883778fa395f9c0cac8aa99fa10d94b1
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-04-28 18:59:43 +0900

    Create 조건문17.c

---
+++ C/Assignment/조건문17.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main (void)
+{
+	char op;
+	int x,y,result;
+	
+	printf("수식을 입력하시오:\n");
+	printf("(예2+5)\n"); 
+	printf(">>");
+	
+	scanf("%d %c %d",&x,&op,&y) ;
+	
+	switch(op)
+	{
+		case '+':
+		{
+			result=x+y;
+			break;
+		}
+		case '-':
+		{
+			result=x-y;
+			break;
+		}
+		case '*':
+		{
+			result=x*y;
+			break;
+		}
+		case '%':
+		{
+			result=x%y;
+			break;
+		}
+		case '&':
+		{
+			result=x&y;
+			break;
+		}
+		case '|':
+		{
+			result=x|y;
+			break;
+		}
+		case '^':
+		{
+			result=x^y;
+			break;
+		}
+		default:
+			printf("지원 되지 않는 연산자입니다.");
+			break;			
+	}
+	printf("%d %c %d = %d",x,op,y,result);
+	return 0;	
+}
