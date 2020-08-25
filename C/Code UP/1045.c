c3e64b1e1404898981a6a09c9ea157f366c5ee7b
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-08-25 17:27:22 +0900

    Create 1045.c

---
+++ C/Code UP/1045.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d\n",a+b);
+	printf("%d\n",a-b);
+	printf("%d\n",a*b);
+	printf("%d\n",a/b);
+	printf("%d\n",a%b);
+	printf("%0.2f\n",(float)a/b);
+	return 0;
+}
