aebfa103c547784e00a6f6582d7e3a3fa70d6299
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-11-18 01:04:25 +0900

    Create BooleanApp.java

---
+++ JAVA/11_17/BooleanApp.java
@@ -0,0 +1,25 @@
+package Java_practice;
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("one");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo ="Hello World";
+		System.out.println(foo);
+		
+		System.out.println("World");
+		System.out.println("egoing");
+		
+		
+		
+		
+
+	}
+
+}
