2c535ef510c08cbcf087e40eec153f2f2cf134c3
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-11-18 01:04:24 +0900

    Create Data_type.java

---
+++ JAVA/11_17/Data_type.java
@@ -0,0 +1,21 @@
+package Java_practice;
+
+public class Data_type {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(6); //number
+		System.out.println("six"); //String
+		System.out.println("6");//String 6
+		//
+		System.out.println(6+6);//12
+		System.out.println("6"+"6");//6
+		System.out.println(6*6);//36
+		System.out.println("1111".length());//길이4
+		//
+		System.out.println("Hello World");//문자열
+		System.out.println('H');//char 문자
+		System.out.println("H");
+	}
+
+}
