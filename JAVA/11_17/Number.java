8cf56f3e35c1f73ac3d079cae293cb900657bbe8
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-11-18 01:04:19 +0900

    Create Number.java

---
+++ JAVA/11_17/Number.java
@@ -0,0 +1,19 @@
+package Java_practice;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(6+2); //8
+		System.out.println(6-2);//4
+		System.out.println(6*2);//12
+		System.out.println(6/2);//3
+		
+		//
+		System.out.println(Math.PI);//3.141592...
+		System.out.println(Math.floor(Math.PI));//소수 내림
+		System.out.println(Math.ceil(Math.PI));// 소수 올림
+
+	}
+
+}
