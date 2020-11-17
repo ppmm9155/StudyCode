2478cf4d41081e95c21d3611be18448b22ee88dc
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-11-18 01:04:22 +0900

    Create String_app.java

---
+++ JAVA/11_17/String_app.java
@@ -0,0 +1,20 @@
+package Java_practice;
+
+public class String_app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println('H');
+		System.out.println("H");
+		
+		System.out.println("Hello"+"World");
+		//
+		System.out.println("Hello \nWorld");
+		
+		//
+		System.out.println("Hello \"World\""); //Hello "World"
+
+	}
+
+}
