8e5f1ae14e52f9729444092fafb58f948fbad07c
박기준 <62309284+ppmm9155@users.noreply.github.com>
2020-11-18 01:04:20 +0900

    Create Variable.java

---
+++ JAVA/11_17/Variable.java
@@ -0,0 +1,24 @@
+package Java_practice;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1; //정수형 1,-1,2,-2,...
+		System.out.println(a);
+		
+		double b =1.1; //double 실수형 1.1,1.123, ...
+		System.out.println(b);
+		
+		String c ="Hello World"; //문자열 String
+		System.out.println(c);
+		
+		float d = 1;
+		System.out.println(d); //정수1 >>>1.0
+		
+		long e = 100; 
+		System.out.println(e);
+
+	}
+
+}
